80b917991d19aac29449834de8d544d742553917
Dev-shirke-27 <devshirke.3@gmail.com>
2025-09-13 15:42:14 +0530

    crate single page for product detail

--- src/main/java/com/sprk/ecommerce/service/ProductService.java
+++ src/main/java/com/sprk/ecommerce/service/ProductService.java
@@ -41,5 +41,16 @@ public class ProductService {
 
         return productRequests;
 
+
+    }
+
+    public ProductRequest getProductById(int productId) {
+        Product product = productRepository.findById(productId).orElse(null);
+
+        if(product != null) {
+            return productMapper.mapProductToProductRequest(product);
+        }
+        return null;
+
     }
 }
