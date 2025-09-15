32089c9c163b057999dc6cceca680b2d039e9bd9
Dev-shirke-27 <devshirke.3@gmail.com>
2025-09-15 14:56:41 +0530

    add update and delete mapping

--- src/main/java/com/sprk/ecommerce/controller/ProductController.java
+++ src/main/java/com/sprk/ecommerce/controller/ProductController.java
@@ -1,4 +1,3 @@
-
 package com.sprk.ecommerce.controller;
 
 import com.sprk.ecommerce.dto.ProductRequest;
@@ -32,20 +31,81 @@ public class ProductController {
     }
 
     @GetMapping("/admin/product")
-    public String showProductForm(Model model){
+    public String showProductForm(Model model) {
         ProductRequest productRequest = new ProductRequest();
         model.addAttribute("productRequest", productRequest);
         return "productform";
     }
 
+    // Show Update Form
+    @GetMapping("/admin/product/{productId}/edit")
+    public String showUpdateProductForm(@PathVariable int productId, Model model, RedirectAttributes redirectAttributes) {
+        ProductRequest productRequest = productService.getProductById(productId);
+        if (productRequest == null) {
+            redirectAttributes.addFlashAttribute("errorMsg", String.format("Product with id = %d not found", productId));
+            return "redirect:/";
+        }
+        model.addAttribute("productRequest", productRequest);
+        return "update-product-form";
+    }
+
+    // Show Update Form
+    @GetMapping("/admin/product/{productId}/delete")
+    public String deleteProductById(@PathVariable int productId, Model model, RedirectAttributes redirectAttributes) {
+        ProductRequest productRequest = productService.getProductById(productId);
+        if (productRequest == null) {
+            redirectAttributes.addFlashAttribute("errorMsg", String.format("Product with id = %d not found", productId));
+            return "redirect:/";
+        }
+
+        boolean result = productService.deleteProduct(productRequest);
+
+        redirectAttributes.addFlashAttribute("errorMsg", String.format("Product with id = %d deleted successfully", productId));
+        return "redirect:/";
+    }
+
+    // Update data
+    @PostMapping("/admin/product/edit")
+    public String processUpdateProductForm(@Valid @ModelAttribute("productRequest") ProductRequest productRequest, BindingResult bindingResult, RedirectAttributes redirectAttributes) {
+
+        if (bindingResult.hasErrors()) {
+//            System.out.println(productRequest);
+            return "productform";
+        } else {
+//            System.out.println(productRequest);
+            boolean result = productService.updateProduct(productRequest);
+            if (result) {
+                String message = "Product: " + productRequest.getProductName() + " has been updated successfully";
+                redirectAttributes.addFlashAttribute("successMsg", message);
+                return "redirect:/product/"+productRequest.getProductId();
+            } else {
+
+                String message = "Some thing wrong happen!!";
+                redirectAttributes.addFlashAttribute("errorMsg", message);
+                return "redirect:/";
+            }
+        }
+    }
+
+    @GetMapping("/product/")
+    public String showErrorForEmptyId(RedirectAttributes redirectAttributes) {
+        redirectAttributes.addFlashAttribute("errorMsg", "Please enter a valid product ID");
+        return "redirect:/";
+    }
+
     // Get Product by id
     @GetMapping("/product/{productId}")
-    public String showIndividualProduct(@PathVariable int productId, Model model, RedirectAttributes redirectAttributes){
+    public String showIndividualProduct(@PathVariable(name = "productId") String productIdStr, Model model, RedirectAttributes redirectAttributes) {
+        if (!productIdStr.matches("^\\d+$")) {
+            redirectAttributes.addFlashAttribute("errorMsg", "product ID must be integer only");
+            return "redirect:/";
+        }
+        int productId = Integer.parseInt(productIdStr);
 
 
         ProductRequest productRequest = productService.getProductById(productId);
-        if(productRequest == null){
-            redirectAttributes.addFlashAttribute("errorMsg", String.format("Product with id = %d not found",productId));
+        if (productRequest == null) {
+            redirectAttributes.addFlashAttribute("errorMsg", String.format("Product with id = %d not found", productId));
             return "redirect:/";
         }
         model.addAttribute("productRequest", productRequest);
@@ -53,29 +113,25 @@ public class ProductController {
     }
 
     @PostMapping("/admin/product")
-    public String processProductForm(@Valid @ModelAttribute("productRequest") ProductRequest productRequest, BindingResult bindingResult, RedirectAttributes redirectAttributes){
+    public String processProductForm(@Valid @ModelAttribute("productRequest") ProductRequest productRequest, BindingResult bindingResult, RedirectAttributes redirectAttributes) {
 
-        if(bindingResult.hasErrors()){
+        if (bindingResult.hasErrors()) {
 //            System.out.println(productRequest);
             return "productform";
-        }
-        else{
+        } else {
 //            System.out.println(productRequest);
             boolean result = productService.saveProduct(productRequest);
-            if(result){
-                String message = "Product: "+productRequest.getProductName()+" has been saved";
-                redirectAttributes.addFlashAttribute("successMsg",message);
+            if (result) {
+                String message = "Product: " + productRequest.getProductName() + " has been saved";
+                redirectAttributes.addFlashAttribute("successMsg", message);
                 return "redirect:/";
-            }else{
+            } else {
 
                 String message = "Some thing wrong happen!!";
-                redirectAttributes.addFlashAttribute("errorMsg",message);
+                redirectAttributes.addFlashAttribute("errorMsg", message);
                 return "redirect:/";
             }
         }
-
-
-
 
 
     }
--- src/main/java/com/sprk/ecommerce/service/ProductService.java
+++ src/main/java/com/sprk/ecommerce/service/ProductService.java
@@ -1,6 +1,7 @@
 package com.sprk.ecommerce.service;
 
 import com.sprk.ecommerce.dto.ProductRequest;
+import com.sprk.ecommerce.entity.Image;
 import com.sprk.ecommerce.entity.Product;
 import com.sprk.ecommerce.mapper.ProductMapper;
 import com.sprk.ecommerce.repository.ProductRepository;
@@ -8,6 +9,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
 
 @Service
@@ -41,16 +43,87 @@ public class ProductService {
 
         return productRequests;
 
-
     }
 
     public ProductRequest getProductById(int productId) {
         Product product = productRepository.findById(productId).orElse(null);
 
-        if(product != null) {
+        if (product != null) {
             return productMapper.mapProductToProductRequest(product);
         }
         return null;
 
+    }
+
+    /*public boolean updateProduct(ProductRequest productRequest) {
+
+        Product product = productMapper.mapProductRequestToProduct(productRequest);// we have product id -> product (id)
+        // In product obj -> we dont have image id -> so it will insert same images again
+        Product dbProduct1 = productRepository.findById(productRequest.getProductId()).orElse(null);
+        List<Image> dbImages1 = dbProduct1.getProductImages();
+        List<Image> productUpdateImages = product.getProductImages();
+        for(int i = 0; i < productUpdateImages.size(); i++) {
+            // Now Image will be updated not inserted
+            productUpdateImages.get(i).setImageId(dbImages1.get(i).getImageId());
+        }
+
+        Product savedProduct = null;
+        savedProduct = productRepository.save(product);
+
+        if (savedProduct != null) {
+            return true;
+        } else {
+            return false;
+        }
+    }*/
+
+    // Bcoz of Image Problem We are writing new logic for update
+    public boolean updateProduct(ProductRequest productRequest) {
+
+        Product dbProduct1 = productRepository.findById(productRequest.getProductId()).orElse(null);
+        List<String> dbImagesUrls = dbProduct1.getProductImages().stream().map(Image::getImagePathUrl).toList();
+        List<String> productRequestImagesUrls = productRequest.getImageUrls();
+
+//        System.out.println("Db Images = "+dbImagesUrls+" Size = "+dbImagesUrls.size());
+//        System.out.println("Product Images = "+productRequestImagesUrls+" Size = "+productRequestImagesUrls.size());
+
+        // Do change images and clear old ones if in productRequest we find new urls
+
+        if (!productRequestImagesUrls.equals(dbImagesUrls) || productRequestImagesUrls.size() != dbImagesUrls.size()) {
+            // work only if there is some changes in image urls
+//            System.out.println("Inside If.....");
+            dbProduct1.getProductImages().clear();// orphan removal
+//            System.out.println("After clear = "+dbProduct1.getProductImages().size());
+            for (String imageUrl : productRequestImagesUrls) {
+
+                Image image = new Image();
+                image.setImagePathUrl(imageUrl);
+                image.setProduct(dbProduct1);
+
+                dbProduct1.getProductImages().add(image);
+
+
+//            System.out.println("After adding new images = "+dbProduct1.getProductImages().size());
+            }
+        }
+
+        // If images are same then I only have to update dbproduct1
+        dbProduct1.setProductName(productRequest.getProductName());
+        dbProduct1.setProductDescription(productRequest.getProductDescription());
+        dbProduct1.setProductPrice(productRequest.getProductPrice().doubleValue());
+        dbProduct1.setBrand(productRequest.getBrand());
+
+        productRepository.save(dbProduct1);
+
+        return true;
+
+
+    }
+
+    public boolean deleteProduct(ProductRequest productRequest) {
+
+        Product dbProduct = productRepository.findById(productRequest.getProductId()).get();
+        productRepository.delete(dbProduct);
+        return true;
     }
 }
